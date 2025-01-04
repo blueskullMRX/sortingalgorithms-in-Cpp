2240e8977379ae96b288132785c766a81693d60b
zakaria amg <142181069+blueskullMRX@users.noreply.github.com>
2025-01-04 16:33:03 +0100

    add selection and insertion sort alg

--- sortingalgorithms/sortingalgorithms.cpp
+++ sortingalgorithms/sortingalgorithms.cpp
@@ -16,25 +16,71 @@ void bubblesort(vector<float> unsortedvect) {
 		{
 			float temp = unsortedvect[j];
 			if (unsortedvect[j] > unsortedvect[j + 1]) {
-				unsortedvect[j] = unsortedvect[j + 1];
-				unsortedvect[j + 1] = temp;
+				swap(unsortedvect[j], unsortedvect[j + 1]);
 			}
 		}
 	}
-	for (int k = 0; k <= unsortedvect.size() - 1; k++) {
+	for (int k = 0; k <= unsortedvect.size() - 1; k++)
+	{
 		cout << *it << " ";
 		it++;
 	}
 	 
 }
 
-void selectionsort(vector<float>)
+void insertionsort(vector<float> unsorted) {
+
+	vector<float>::iterator it = unsorted.begin();
+	for (int i = 1; i < unsorted.size(); i++) {
+		float key = unsorted[i];
+		int k = i - 1;
+		//shifting the sorted part for the key right position 
+		while (k >= 0 && unsorted[k] > key) 
+		{
+			unsorted[k + 1] = unsorted[k];
+			k--;
+		}
+		unsorted[k + 1] = key;
+	}
+
+	for (int k = 0; k <= unsorted.size() - 1; k++)
+	{
+		cout << *it << " ";
+		it++;
+	}
+	
+}
+void selectionsort(vector<float> unsorted) {
+	
+	for (int i = 0; i < unsorted.size() - 1; i++) {
+		int minIndex = i;
+
+		for (int j = i + 1; j < unsorted.size(); j++) {
+			if (unsorted[j] < unsorted[minIndex]) {
+				minIndex = j;
+			}
+		}
+
+		if (minIndex != i) {
+			swap(unsorted[i], unsorted[minIndex]);
+		}
+	}
+	vector<float>::iterator ss = unsorted.begin();
+		cout << endl;
+		for (int f = 0; f <= unsorted.size() - 1; f++) {
+			cout << *ss << " ";
+			ss++;
+		}
+}
 int main()
 {
-	vector<float> vect = { 55, 24, 33, 44, 55,48,98,12,46,03,487,6,35 };
-	bubblesort(vect);
-
-	
-
+	vector<float> vect = { 55, 24, 33, 44, 55,48,98,12,46,2,487,6,35 };
+	selectionsort(vect);
+	/*vector<float>::iterator ss = vect.begin();*/
+	/*cout << endl;
+	for (int f = 0; f <= vect.size() - 1; f++) {
+		cout << *ss << " ";
+		ss++;
+	}*/
 }
 
